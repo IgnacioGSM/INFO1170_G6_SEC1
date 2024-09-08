fb9c8744919bf3761c58a6599c0dad08f705fd43
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-09-08 18:31:28 -0300

    Imagenes de marcadores personalizadas agregadas

--- script_mapa.js
+++ script_mapa.js
@@ -5,7 +5,7 @@ var map = L.map('map').setView([-38.734547, -72.589724], 13);   // Creacion del 
         }).addTo(map);
 
 
-        var popup = L.popup();
+        var popup = L.popup();                                     // Creacion de un popup
 
         function onMapClick(e) {                                   // Funcion que muestra un popup con Latitud/Longitud al hacer click en el mapa
         popup
@@ -14,3 +14,24 @@ var map = L.map('map').setView([-38.734547, -72.589724], 13);   // Creacion del 
             .openOn(map);
         }
         map.on('click', onMapClick);
+
+
+        var HospitalIcon = L.Icon.extend({      // Todos los objetos del tipo HospitalIcon tendran las mismas propiedades
+            options: {
+                shadowUrl: "imagenes/marcador_sombra.png",
+                iconSize: [69, 93],
+                shadowSize: [68, 67],
+                iconAnchor: [34,92],           // Punto del icono correspondiente a la ubicacion del marcador
+                shadowAnchor: [1, 67],
+                popupAnchor: [0, -93]             // Punto donde se abre el popup
+            }
+        });
+
+        var MarcadorRojo = new HospitalIcon({iconUrl: "imagenes/marcador_rojo.png"});
+        var MarcadorVerde = new HospitalIcon({iconUrl: "imagenes/marcador_verde.png"});
+        var MarcadorAmarillo = new HospitalIcon({iconUrl: "imagenes/marcador_amarillo.png"});
+
+
+        var marker = L.marker([-38.736703, -72.610633], {icon: MarcadorRojo}).addTo(map);  // Creacion de un marcador
+
+        marker.bindPopup("<b>Clinica Alemana</b>");  // Popup del marcador
