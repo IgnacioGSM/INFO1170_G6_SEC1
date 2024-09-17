147c937621c73ddedadad14ba18f45141421b989
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-09-17 15:09:46 -0300

    Modificaciones al entrar como user, admin o recepcionista + clases para la visibilidad de ciertos items

--- scripts/index/seleccion_usertype.js
+++ scripts/index/seleccion_usertype.js
@@ -7,10 +7,35 @@ document.getElementById("index-seleccion-usertype").addEventListener("change", f
     document.getElementById("overlay").style.display = "none";
     if (usertype == "invitado"){
         console.log("invitado");
-        document.getElementById("index-formulario-invitado").style.display = "flex";
+        let invitado_elements = document.getElementsByClassName("invitado-only");
+        for (let i = 0; i < invitado_elements.length; i++){
+            if (invitado_elements[i].tagName == "LI"){
+                invitado_elements[i].style.display = "inline";
+            }
+            else{
+                invitado_elements[i].style.display = "flex";
+            }
+        }
     }
-
-    if (usertype == "suspendido"){
+    else if (usertype == "recepcionista"){
+        console.log("recepcionista");
+        let recepcionista_elements = document.getElementsByClassName("recepcionista-only");
+        for (let i = 0; i < recepcionista_elements.length; i++){
+            if (recepcionista_elements[i].tagName == "LI"){
+                recepcionista_elements[i].style.display = "inline";
+            }
+        }
+    }
+    else if (usertype == "admin"){
+        console.log("admin");
+        let admin_elements = document.getElementsByClassName("admin-only");
+        for (let i = 0; i < admin_elements.length; i++){
+            if (admin_elements[i].tagName == "LI"){
+                admin_elements[i].style.display = "inline";
+            }
+        }
+    }
+    else if (usertype == "suspendido"){
         console.log("suspendido");
         document.getElementById("index-formulario-suspendido").style.display = "flex";
     }
