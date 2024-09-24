a8293d0c87e8ff6c3228ac07c72692652f7a00ca
TOMAS-PENROZ-M <tpenroz2024@alu.uct.cl>
2024-09-24 18:11:20 -0300

    validacion simple de la configuracion y ajuste a las estrellas

--- perfilUsuario.js
+++ perfilUsuario.js
@@ -20,3 +20,29 @@ function mostrarSeccion(id){
 
     document.getElementById(id).style.display = 'block';
 }
+
+function manejaConfi(event, ContraseñaInputID, mensajeExito){
+    event.preventDefault();
+
+    const contraseñaIngresada = document.getElementById(ContraseñaInputID).value;
+
+    validarContraseña(contraseñaIngresada, function(isValid){
+        if (isValid){
+            alert(mensajeExito);
+        }else {
+            alert('Contraseña incorrecta intente de nuevo');
+        }
+    });
+}
+
+document.getElementById('CambiarCorreo').addEventListener('submit', function(event){
+    manejaConfi(event, 'confirmarCorreo', 'correo actualizado con exito');
+});
+
+document.getElementById('CambiarTelefono').addEventListener('submit', function(event){
+    manejaConfi(event, 'confirmarTelefono', 'telefono actualizado con exito');
+});
+
+document.getElementById('CambiarDireccion').addEventListener('submit', function(event){
+    manejaConfi(event, 'confirmarDireccion', 'direccion actualizado con exito');
+});
