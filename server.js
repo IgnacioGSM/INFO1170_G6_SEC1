e825f31192b40a1728a645566e80e07fdf1f2b4a
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-10-08 21:22:04 -0300

    Mas cambios en relación a los directorios usados y rutas

--- server.js
+++ server.js
@@ -13,6 +13,32 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'views','index.html'));
 });
 
+// Rutas para páginas a las que se accede desde index, direcciones temporales hasta que todas estén bien organizadas
+app.get('/iniciosesion', (req, res) => {
+  res.sendFile(path.join(__dirname,'iniciosesion.html'));
+});
+
+app.get('/registropersona', (req, res) => {
+  res.sendFile(path.join(__dirname,'registropersona.html'));
+});
+
+app.get('/perfilUsuario', (req, res) => {
+  res.sendFile(path.join(__dirname,'perfilUsuario.html'));
+});
+
+app.get('/mis_solicitudes', (req, res) => {
+  res.sendFile(path.join(__dirname,'mis_solicitudes.html'));
+});
+
+app.get('/admin', (req, res) => {
+  res.sendFile(path.join(__dirname,'admin.html'));
+});
+
+app.get('/inter_recepcionista', (req, res) => {
+  res.sendFile(path.join(__dirname,'inter_recepcionista.html'));
+});
+
+
 app.post('/submit_solicitud', (req, res) => {
     console.log(req.body);
     res.sendFile(path.join(__dirname,'views','index.html'));
