0c5ae83f50ca0e8d1a50bcfae138f2733bfddc7e
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-10-15 17:01:57 -0300

    Pagina index usando EJS y gestión de sesiones básico

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const path = require('path');
-const app = express();
+const session = require('express-session');
 const { host, user, password, database } = require('./credenciales_mysql.js');  // cambiar los datos en el archivo credenciales_mysql.js para que funcione en sus equipos
+
+const app = express();
 
 // Conexión a la base de datos
 const db = mysql.createConnection({
@@ -13,18 +15,47 @@ const db = mysql.createConnection({
   database: database
 });
 
+// Configuraciones de la app
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(session({
+  secret: 'clave-super-secreta',
+  resave: false,
+  saveUninitialized: false,
+  cookie : { maxAge: 120000 }
+}));
+
+// Archivos estaticos
 app.use(express.static(path.join(__dirname,'public')));
+
+// Configuración de la plantilla
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Ruta principal
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'views','index.html'));
+  if (req.session.usuario) {
+    res.render('index', {user: req.session.usuario});
+  } else {
+    res.render('index', {user: 0});
+  }
 });
 
 // Rutas para páginas a las que se accede desde index, direcciones temporales hasta que todas estén bien organizadas
 app.get('/iniciosesion', (req, res) => {
-  res.sendFile(path.join(__dirname,'iniciosesion.html'));
+  // Para testear, al entrar a iniciosesion se simula que se inicia sesión como usuario
+  let logQuery = "SELECT * FROM Usuario WHERE RUT = ?";
+  db.query(logQuery, ['123456789'], (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    if (result.length === 0) {
+      res.send("Usuario no encontrado");
+    } else {
+      req.session.usuario = result[0];
+      res.sendFile(path.join(__dirname,'iniciosesion.html')); // Envío de respuestas DENTRO de la función del query, para que no se envíe antes de que se ejecute la consulta
+    }
+  });
 });
 
 app.get('/registropersona', (req, res) => {
@@ -80,12 +111,17 @@ app.post('/submit_solicitud', (req, res) => {
                     console.log(err);
                 } else {
                     console.log("Solicitud enviada");
-                    res.sendFile(path.join(__dirname,'views','index.html'));
+                    res.render('index', {user: req.session.usuario});
                 }
             });
         }
       });
     });
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
 
 // Escuchar en el puerto 3000
 app.listen(3000, () => {
