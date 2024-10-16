8842e73c63945b944d06d75b68344d1c22ee9f71
TOMAS-PENROZ-M <tpenroz2024@alu.uct.cl>
2024-10-15 22:09:31 -0300

    cambios generales JSregistro

--- JSregistropersonal.js
+++ JSregistropersonal.js
@@ -2,21 +2,28 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const mysql = require('mysql2');
 const app = express();
+const path = require('path');
+
 app.use(express.json());
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password',
-    database: 'Hospitrack'
+    password: '',
+    database: 'hospitrack'
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 
 // Registro de usuario
 app.post('/register', async (req, res) => {
-    const { RUT, Nombre, Apellido, CorreoElectronico, NumeroTelefono, TipoUsuario, Contrasenia } = req.body;
+    const { RUT, Nombre, CorreoElectronico, Contrasenia } = req.body;
 
     // Verificar si el correo ya existe
-    connection.query('SELECT * FROM Usuario WHERE CorreoElectronico = ?', [CorreoElectronico], async (err, results) => {
+    connection.query('SELECT * FROM usuario WHERE CorreoElectronico = ?', [CorreoElectronico], async (err, results) => {
         if (err) return res.status(500).send('Error en el servidor');
         if (results.length > 0) return res.status(400).send('El correo ya está registrado');
 
@@ -24,8 +31,8 @@ app.post('/register', async (req, res) => {
         const hashedPassword = await bcrypt.hash(Contrasenia, 10);
 
         // Insertar nuevo usuario
-        connection.query('INSERT INTO Usuario (RUT, Nombre, Apellido, CorreoElectronico, NumeroTelefono, TipoUsuario, Contrasenia) VALUES (?, ?, ?, ?, ?, ?, ?)', 
-            [RUT, Nombre, Apellido, CorreoElectronico, NumeroTelefono, TipoUsuario, hashedPassword], 
+        connection.query('INSERT INTO usuario (RUT, Nombre, CorreoElectronico, Contrasenia) VALUES (?, ?, ?, ?)', 
+            [RUT, Nombre, CorreoElectronico, hashedPassword], 
             (err, result) => {
                 if (err) return res.status(500).send('Error al registrar el usuario');
                 res.status(201).send('Usuario registrado exitosamente');
@@ -35,5 +42,5 @@ app.post('/register', async (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('Servidor escuchando en puerto 3000');
+    console.log('Servidor escuchando en puerto 3000: http://localhost:3000');
 });
