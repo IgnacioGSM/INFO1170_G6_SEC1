b43fb5e0a7927966e2b2555316c847a0480b9b64
martinchinaski <mmaier2024@alu.uct.cl>
2024-10-23 12:37:44 -0300

    fusion main

--- server.js
+++ server.js
@@ -32,32 +32,17 @@ app.use(express.static(path.join(__dirname,'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Ruta principal
+const indexRouter = require('./rutas/index');
+
+app.use('/', indexRouter);
 
 
+// Ruta Mis Solicitudes
+const misSolicitudesRouter = require('./rutas/missolicitudes.js');
 
-// Ruta para la página principal que redirige al panel de administración
-app.get('/', (req, res) => {
-  if (req.session.usuario) {
-    res.render('index', {user: req.session.usuario});
-  } else {
-    res.render('index', {user: 0});
-  }
-});
+app.use('/mis_solicitudes', misSolicitudesRouter);
 
-app.get('/mis_solicitudes', (req, res) => {
-  if (req.session.usuario) {
-    let querySolicitudes = "SELECT * FROM Solicitud WHERE IdUsuario = ?";
-    db.query(querySolicitudes, [req.session.usuario.IdUsuario], (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-      res.render('mis_solicitudes', {user: req.session.usuario, solicitudes: result});
-      }
-    });
-  } else {
-    res.render('index', {user: 0}); // No se debería poder acceder a esta página sin estar logeado
-  }
-});
 
 // Rutas para páginas a las que se accede desde index, direcciones temporales hasta que todas estén bien organizadas
 app.get('/iniciosesion', (req, res) => {
@@ -71,7 +56,7 @@ app.get('/iniciosesion', (req, res) => {
       res.send("Usuario no encontrado");
     } else {
       req.session.usuario = result[0];
-      res.sendFile(path.join(__dirname,'views','iniciosesion.html')); // Envío de respuestas DENTRO de la función del query, para que no se envíe antes de que se ejecute la consulta
+      res.sendFile(path.join(__dirname,'views','iniciosesion.html'));
     }
   });
 });
@@ -105,45 +90,6 @@ app.get('/inter_recepcionista', (req, res) => {
   res.sendFile(path.join(__dirname,'inter_recepcionista.html'));
 });
 
-
-app.post('/submit_solicitud', (req, res) => {
-    console.log(req.body);
-    let idSeccion = 1;
-    let date = new Date();
-    let hora_solicitud = date.getFullYear() + '/' +
-      ('0' + (date.getMonth() + 1)).slice(-2) + '/' +
-      ('0' + date.getDate()).slice(-2) + ' ' +
-      ('0' + date.getHours()).slice(-2) + ':' +
-      ('0' + date.getMinutes()).slice(-2) + ':' +
-      ('0' + date.getSeconds()).slice(-2);
-    let estado = 'pendiente';
-
-    let rut = req.body.rut.replace(/[^\dkK]/g, ''); // Quita puntos y guión recibidos en el rut
-    let correo = req.body.correo;
-    let motivo_consulta = req.body.motivo_consulta;
-
-    let queryUser = "SELECT * FROM Usuario WHERE RUT = ?";
-    db.query(queryUser, [rut], (err, result) => {
-        if (err) {
-            console.log(err);
-        }
-        if (result.length === 0) {
-            console.log("Usuario no encontrado");
-            res.send("Usuario no encontrado");
-        } else {
-            let idUsuario = result[0].IdUsuario;
-            let querySolicitud = "INSERT INTO Solicitud (IdUsuario, IdSeccion, Mensaje, HoraSolicitud, Estado) VALUES (?, ?, ?, ?, ?)";
-            db.query(querySolicitud, [idUsuario, idSeccion, motivo_consulta, hora_solicitud, estado], (err, result) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("Solicitud enviada");
-                    res.render('index', {user: req.session.usuario});
-                }
-            });
-        }
-      });
-    });
 
 
 // Ruta para el panel de administrador
@@ -214,12 +160,6 @@ app.post('/eliminar_hospital/:id', (req, res) => {
     }
     res.redirect('/admin');
   });
-});
-
-
-app.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.redirect('/');
 });
 
 // Escuchar en el puerto 3000
