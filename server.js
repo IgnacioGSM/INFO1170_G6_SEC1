9c1d81709f8ac4a9430f574c6e1c765e18b94636
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-11-04 13:19:58 -0300

    Actualizar correo, telefono y contraseña funcionales en perfil de usuario

--- server.js
+++ server.js
@@ -8,15 +8,7 @@ const { rmSync } = require('fs');
 
 const app = express();
 
-// Configuración de multer
-const storage = multer.diskStorage({
-  destination: function(req, file, cb){
-      cb(null, 'uploads/'); // Carpeta donde se almacenarán los archivos
-  },
-  filename: function(req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname); // Nombre del archivo
-  }
-});
+
 
 // La conexión a la base de datos se encuentra en database.js
 const db = require('./database');
@@ -28,7 +20,7 @@ app.use(session({
   secret: 'clave-super-secreta',
   resave: false,
   saveUninitialized: false,
-  cookie : { maxAge: 120000 }
+  cookie : { maxAge: 1200000 }
 }));
 
 // Archivos estaticos
@@ -40,31 +32,30 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Ruta principal
 const indexRouter = require('./rutas/index');
-
 app.use('/', indexRouter);
 
 
 // Ruta Mis Solicitudes
 const misSolicitudesRouter = require('./rutas/missolicitudes.js');
-
 app.use('/mis_solicitudes', misSolicitudesRouter);
 
 
 // Ruta inicio de sesión
 const loginRouter = require('./rutas/login');
-
 app.use('/login', loginRouter);
 
 
 // Ruta registro
 const registroRouter = require('./rutas/registro');
-
 app.use('/registro', registroRouter);
 
 // Ruta admin
 const adminRouter = require('./rutas/admin');
-
 app.use('/admin', adminRouter);
+
+// Ruta perfil
+const perfilRouter = require('./rutas/perfil');
+app.use('/perfilUsuario', perfilRouter);
 
 
 // Cambiar contraseña (olvidé mi contraseña)
@@ -87,9 +78,8 @@ app.post('/forgot-password', async (req, res) => {
 
 
 // -------- PERFIL DE USUARIO --------
-const upload = multer({storage:storage});
 
-app.post('/envexp', upload.array('archivo', 5), (req, res) =>{
+/*app.post('/envexp', upload.array('archivo', 5), (req, res) =>{
     const userId = 4;
     const archivos = req.files;
 
@@ -97,7 +87,7 @@ app.post('/envexp', upload.array('archivo', 5), (req, res) =>{
         return res.status(400).send('No se subio ningun archivo');
     }
     archivos.forEach((archivo) =>{
-        const query = 'INSERT INTO expedientes_medicos (IdUsuario, nombre_archivo, ruta_archivo) VALUES (?, ?, ?)';
+        const query = 'INSERT INTO expedientes_medicos (idusuario, nombre_archivo, ruta_archivo) VALUES (?, ?, ?)';
         db.query(query, [userId, archivo.originalname, archivo.path], (err, result) =>{
             if(err){
                 console.error('Error al guardar el archivo en la base de datos', err);
@@ -106,54 +96,6 @@ app.post('/envexp', upload.array('archivo', 5), (req, res) =>{
         });
     });
     res.send('Expedientes medicos subidos con exito');
-});
-
-
-app.post('/cambiarcorreo', (req, res) => {
-    const {nuevoCorreo, confirmarCorreo} = req.body;
-    const userId = 4;
-
-    db.query('SELECT contrasenia FROM Usuario WHERE idusuario = ?', [userId], (error, results) => {
-        if (error) return res.status(500).send('Error en el servidor');
-
-        const storedPassword = results[0].contrasenia;
-        if (confirmarCorreo == storedPassword) {
-            bd.query('UPDATE Usuario SET correoelectronico = ? WHERE idusuario = ?', [nuevoCorreo, userId], (error) =>{
-                if (error) return res.status(500).send('Error al actualizar el correo');
-                res.send('Correo actualizado');
-            });
-        } else {
-            res.send("Contraseña incorrecta");
-        }
-        /*bcrypt.compare(confirmarCorreo, storedPassword, (err, isMatch) => {
-            if (err) return res.status(500).send('Error en el servidor');
-            if (!isMatch) return res.status(400).send('Contraseña incorrecta');
-
-            connection.query('UPDATE usuarios SET CorreoElectronico = ? WHERE IdUsuario = ?', [nuevoCorreo, userId], (error) =>{
-                if (error) return res.status(500).send('Error al actualizar el correo');
-                res.send('Correo actualizado');
-            });
-        });*/
-    });
-});
-
-app.post('/cambiartelefono', (req, res) => {
-    const {nuevoTelefono, confirmarTelefono} = req.body;
-    const userId = 4;
-
-    db.query('SELECT contrasenia FROM Usuario WHERE idusuario = ?', [userId], (error, results) => {
-        if (error) return res.status(500).send('Error en el servidor');
-
-        const storedPassword = results[0].contrasenia;
-        if (confirmarTelefono == storedPassword) {
-            db.query('UPDATE usuario SET NumeroTelefono = ? WHERE idusuario = ?', [nuevoTelefono, userId], (error) =>{
-                if (error) return res.status(500).send('Error al actualizar el telefono');
-                res.send('Telefono actualizado');
-            });
-        } else {
-            res.send("Contraseña incorrecta");
-        }
-    });
 });
 
 app.post('/cambiardireccion', (req, res) => {
@@ -173,51 +115,7 @@ app.post('/cambiardireccion', (req, res) => {
             res.send("Contraseña incorrecta");
         }
     });
-});
-
-app.post('/cambiarcontraseña', (req, res) => {
-    const {nuevaContraseña, confirmarContraseña, contraseñaActual} = req.body;
-    const userId = 4;
-
-    db.query('SELECT contrasenia FROM Usuario WHERE idusuario = ?', [userId], (error, results) => {
-        if (error) return res.status(500).send('Error en el servidor');
-
-        const storedPassword = results[0].contrasenia;
-        if (contraseñaActual === storedPassword) {
-
-            if (nuevaContraseña === confirmarContraseña) {
-
-                db.query('UPDATE Usuario SET Contrasenia = ? WHERE idusuario = ?', [nuevaContraseña, userId], (error) =>{
-                    if (error) return res.status(500).send('Error al actualizar la contraseña');
-                    res.send('Contraseña actualizada');
-                });
-            } else {
-                res.send("Contraseña incorrecta");
-            }
-        } else {
-            res.send('La contraseña actual no coincide')
-        }
-    }); 
-});
-
-app.get('/perfilUsuario', (req,res) =>{
-    const userId = 4;
-
-    const query = 'SELECT nombre, correoElectronico, rut, numerotelefono FROM Usuario WHERE idusuario = ?';
-
-    db.query(query, [userId], (err, result) =>{
-        if (err){
-            console.error('Error al obtener los datos del usuario: ', err);
-            return res.status(500).send('Error al obtener los datos del usuario');
-        }
-
-        if (result.length > 0){
-            res.render('perfilUsuario', {usuario: result[0]});
-        }else{
-            res.status(404).send('Usuario no encontrado');
-        }
-    });
-});
+});*/
 
 // Escuchar en el puerto 3000
 app.listen(3000, () => {
