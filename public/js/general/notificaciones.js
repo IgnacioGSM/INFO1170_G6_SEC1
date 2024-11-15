39188273c8fd48ff3a4fe049ffeff8f34368f486
Ignacio G. <162763971+IgnacioGSM@users.noreply.github.com>
2024-11-14 22:09:53 -0300

    Al cargar las notificaciones ahora debería crear links hacia la solicitud en caso de encontrar

--- public/js/general/notificaciones.js
+++ public/js/general/notificaciones.js
@@ -11,8 +11,21 @@ function socketListo(socket) {
             notificaciones_list.innerHTML = "<li class='dropdown-item disabled'>No hay notificaciones</li>";
             console.log("No hay notificaciones");
             return;
+        } else {
+            for (let i = 0; i < notificaciones.length; i++) {
+                let notif = notificaciones[i];
+                // Obtener id de la solicitud respondida
+                fetch('/id-soli-from-respuesta?idrespuesta=' + notif.idrespuesta)
+                .then(response => response.json())
+                .then(data => {
+                    let idSoli = data.idsoli;
+                    let notif_element = document.createElement("li");
+                    notif_element.className = "dropdown-item";
+                    notif_element.innerHTML = "<a href='/mis_solicitudes/solicitud?idsoli=" + idSoli + "'><span class='d-inline-block bg-success rounded-circle p-1'></span>" + notif.mensaje + "</a>";
+                    notificaciones_list.appendChild(notif_element);
+                });
+            }
         }
-        // Falta cuando sí hay notificaciones ...
     });
 }
 
--- rutas/missolicitudes.js
+++ rutas/missolicitudes.js
@@ -4,7 +4,7 @@ const db = require('../database.js');
 
 router.get('/', (req, res) => { // /mis_solicitudes
     if (req.session.usuario) {
-      let querySolicitudes = "SELECT * FROM Solicitud WHERE IdUsuario = ?";
+      let querySolicitudes = "SELECT * FROM Solicitud WHERE idusuario = ?";
       db.query(querySolicitudes, [req.session.usuario.IdUsuario], (err, result) => {
         if (err) {
           console.log(err);
@@ -17,56 +17,70 @@ router.get('/', (req, res) => { // /mis_solicitudes
     }
   });
 
+// Obtener id de la solicitud respondida con el id de la respuesta
+router.get('/id-soli-from-respuesta', (req, res) => { // /mis_solicitudes/id-soli-from-respuesta
+  let respuesta = req.query.idrespuesta;
+  let querySolicitud = "SELECT idsolicitud FROM RespuestaSolicitud WHERE idrespuesta = ?";
+  db.query(querySolicitud, [respuesta], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.json({idsoli: 'error'});
+    } else {
+      res.json({idsoli: result[0].idsolicitud});
+    }
+  });
+});
+  
 router.get('/solicitud', (req, res) => {    // /mis_solicitudes/solicitud
-    console.log("Entrando a la solicitud: " + req.query.idsoli);  // asi se recibe el id de la solicitud enviado desde la url
-    if (req.session.usuario) {
-      let queryPaginaSolicitud = "SELECT soli.*, sec.NombreSeccion, cen.Nombre AS NombreHospital \
-                                  FROM Solicitud soli \
-                                  INNER JOIN Seccion sec ON soli.IdSeccion = sec.IdSeccion \
-                                  INNER JOIN CentroSalud cen ON cen.IdCentro = sec.IdCentro \
-                                  WHERE soli.IdSolicitud = ?";
-      db.query(queryPaginaSolicitud, [req.query.idsoli], (err, result) => {
+  console.log("Entrando a la solicitud: " + req.query.idsoli);  // asi se recibe el id de la solicitud enviado desde la url
+  if (req.session.usuario) {
+    let queryPaginaSolicitud = "SELECT soli.*, sec.nombreseccion, cen.nombre AS nombrehospital \
+                  FROM Solicitud soli \
+                  INNER JOIN Seccion sec ON soli.idseccion = sec.idseccion \
+                  INNER JOIN CentroSalud cen ON cen.idcentro = sec.idcentro \
+                  WHERE soli.idsolicitud = ?";
+    db.query(queryPaginaSolicitud, [req.query.idsoli], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result[0].estado === 'pendiente') {
+      res.render('solicitud', {user: req.session.usuario, solicitud: result[0], respuesta: 0});
+      }
+      else {
+      let queryRespuesta = "SELECT res.*, rec.* FROM RespuestaSolicitud res \
+                  INNER JOIN Usuario rec ON res.idusuario = rec.idusuario \
+                  WHERE idsolicitud = ?";
+      db.query(queryRespuesta, [req.query.idsoli], (err, result2) => {
         if (err) {
-          console.log(err);
+        console.log(err);
         } else {
-          if (result[0].Estado === 'pendiente') {
-            res.render('solicitud', {user: req.session.usuario, solicitud: result[0], respuesta: 0});
-          }
-          else {
-            let queryRespuesta = "SELECT res.*,rec.* FROM RespuestaSolicitud res \
-                                  INNer JOIN Usuario rec ON res.IdUsuario = rec.IdUsuario \
-                                  WHERE IdSolicitud = ?";
-            db.query(queryRespuesta, [req.query.idsoli], (err, result2) => {
-              if (err) {
-                console.log(err);
-              } else {
-                if (result2.length === 0) { // Si no hay respuesta en la base de datos
-                  res.render('solicitud', {user: req.session.usuario, solicitud: result[0], respuesta: 0});
-                } else {
-                  res.render('solicitud', {user: req.session.usuario, solicitud: result[0], respuesta: result2[0]});
-                }
-              }
-            });
-          }
+        if (result2.length === 0) { // Si no hay respuesta en la base de datos
+          res.render('solicitud', {user: req.session.usuario, solicitud: result[0], respuesta: 0});
+        } else {
+          res.render('solicitud', {user: req.session.usuario, solicitud: result[0], respuesta: result2[0]});
+        }
         }
       });
-    } else {
-      res.redirect('/');  // Si no está logeado, redirige a la página principal
+      }
     }
+    });
+  } else {
+    res.redirect('/');  // Si no está logeado, redirige a la página principal
+  }
   });
   
 router.get('/cancelarsolicitud', (req, res) => { // /mis_solicitudes/cancelarsolicitud
-    if (req.session.usuario) {
-      let queryCancel = "DELETE FROM Solicitud WHERE IdSolicitud = ?";
-      db.query(queryCancel, [req.query.idsoli], (err, result) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Solicitud cancelada");
-          res.redirect('/mis_solicitudes');
-        }
-      });
+  if (req.session.usuario) {
+    let queryCancel = "DELETE FROM Solicitud WHERE idsolicitud = ?";
+    db.query(queryCancel, [req.query.idsoli], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Solicitud cancelada");
+      res.redirect('/mis_solicitudes');
     }
+    });
+  }
   });
 
 module.exports = router;
